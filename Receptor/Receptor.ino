00748fa6d8274b59b26ae5903bc9e054e8f75076
Guihgo <guihgo100milha@gmail.com>
2021-01-05 22:20:08 -0300

    refactor code

--- Receptor/Receptor.ino
+++ Receptor/Receptor.ino
@@ -1,36 +1,34 @@
-#include <RHReliableDatagram.h>                      // biblioteca Radiohead reliableDatagram
-#include <RH_ASK.h>                                  // biblioteca Radiohead ASK
-#include <SPI.h>                                     // biblioteca SPI
- 
-#define TX_ADDRESS 12                                 // endereço do transmissor
-#define RX_ADDRESS 13                                 // endereço do receptor 
- 
-uint8_t count = 0;                                   // contador
-uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];                 // buffer da mensagem
-uint8_t tamanho;                                     // tamanho da mensagem
-uint8_t from;                                        // endereço de quem transmite
- 
-RH_ASK driver;                                       // instância RH ASK
-RHReliableDatagram gerente(driver, RX_ADDRESS);      // configurando o gerenciador
- 
-void setup()
-{
-  Serial.begin(9600);                                // inicializa console serial 9600 bps
-  if (!gerente.init())                               // se a inicialização do gerenciador falhar
-    Serial.println("Falha na inicializacao");        // print na console serial
-}
- 
-void loop()
-{
-  if (gerente.available())                           // se gerenciador estiver ativo
-  {
-    tamanho = sizeof(buf);                           // determina o tamanho do buffer
-    if (gerente.recvfromAck(buf, &tamanho, &from))   // se o gerenciador receber mensagem
-    {
-      Serial.print("Recebido de: 0x");               // print na console serial
-      Serial.print(from, HEX);                       // print do endereço do transmissor em Hexadecimal
-      Serial.print(": ");                            // print na console serial
-      Serial.println((char*)buf);                    // print da mensagem recebida
+#include <RH_ASK.h>
+
+#define RF_RX_PIN 13
+#define RF_TX_PIN 12
+
+RH_ASK RF_driver(5000, RF_RX_PIN, RF_TX_PIN, 0);
+
+void setup() {
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    if (RF_driver.init()) {
+        Serial.println("RF init started!");
+    } else {
+        Serial.println("RF init failed!");
     }
-  }
+}
+
+unsigned long _cont = 0;
+uint8_t RF_messageReceived[RH_ASK_MAX_MESSAGE_LEN];
+uint8_t RF_messageReceived_len;
+
+void loop() {
+    char msg[27];
+
+    sprintf(msg, "valor: %ld", _cont++);
+
+    Serial.print("RF packet: sending...");
+    Serial.println(msg);
+    RF_driver.send((uint8_t *)msg, strlen(msg));
+    RF_driver.waitPacketSent();
+    Serial.println("RF packet: sent!");
+    delay(200);
+
 }
